d4a5e1342e791f879b96fb61db002c170cb43aa0
ad_victoriam_101 <keeper24@gmail.com>
2021-12-22 13:35:25 -0800

    created model and pushed model h5 to data folder

---
+++ model.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+from kears.models import load_model
+
+model = keras.models.load_model("../kickstarter3/data/my_model.h5")
+
+model.compile(model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['mse']))
+
+#given imputs take test df
+def user_input(user_campaign):
+    X_features = user_campaign[['launch_to_deadline_days', 'staff_pick', 'pledged', 'backers_count', 'spotlight', 'goal']]
+    return model.predict(X_features)
+
+user_input
